ed8095ddd12909a41fa363e0b202de4ecb7e5707
Josh <dash219@gmail.com>
2019-05-03 10:48:29 -0700

    Added bullet object

---
+++ Assets/Scripts/MegaBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaBullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    public enum shotDirsEnum {East = 1, West = -1, North = 2, South = -2};
+    public int shotDir;
+
+    void Awake()
+    {
+        bulletSpeed = 0.015f;
+        shotDir = (int)shotDirsEnum.East;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shotDir == 1 || shotDir == -1)
+        {
+            transform.Translate(bulletSpeed * shotDir, 0, 0);
+        }
+        if (shotDir == 2 || shotDir == -2)
+        {
+            transform.Translate(0, bulletSpeed * (shotDir/2), 0);
+        }
+    }
+}
