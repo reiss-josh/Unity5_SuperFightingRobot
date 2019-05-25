1b19e10e65d122e3dd3ffa37c3c5d6be7170ef51
Josh <dash219@gmail.com>
2019-05-24 21:53:56 -0700

    update for tilemap

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform LookAt;
+
+    public float boundX = 2.0f;
+    public float boundY = 1.5f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        float dx = LookAt.position.x - transform.position.x;
+        if(dx > boundX || dx < -boundX)
+        {
+            if(transform.position.x < LookAt.position.x)
+            {
+                delta.x = dx - boundX;
+            }
+            else
+            {
+                delta.x = dx + boundX;
+            }
+        }
+
+        transform.position = transform.position + delta;
+    }
+}
